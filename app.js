f3770c58004bc4d6073b03032360c0cc475b7a78
Ravi20-05 <sirviravi36@gmail.com>
2025-05-11 22:29:33 +0530

    First version 0.1

--- app.js
+++ app.js
@@ -94,6 +94,6 @@ app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/views/contact.html');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
